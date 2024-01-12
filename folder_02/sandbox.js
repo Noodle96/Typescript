55880df0c04b35befbd995f42cf95920ee2ca31b
Noodle96 <jorgealfredomarino2@gmail.com>
2024-01-12 13:19:51 -0500

    declracion de una variable, compile .ts file

---
+++ folder_02/sandbox.js
@@ -0,0 +1,7 @@
+// in terminal tsc nameFile.ts -w
+var b = 'Elias';
+console.log(b);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ folder_02/sandbox.ts
@@ -0,0 +1,10 @@
+// in terminal tsc nameFile.ts -w
+const b = 'Elias';
+
+console.log(b);
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  console.log(input);
+});
